5d25d094620ce8d169cf9392a87a803ceda863ea
Pravat <pravatkiran7@gmail.com>
2024-07-29 16:21:33 +0545

    basic layout with navbar and homepage created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import HomePage from './pages/HomePage'
+import JobPage from './pages/JobPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+     <Route index element={<HomePage />} />
+     <Route path='/jobs' element={<JobPage />} />
+     <Route path='*' element={<NotFoundPage />} />
+  </Route>
+)
+)
 
 const App = () => {
-  return (
-    <div className='text-5xl'>App</div>
-  )
+  return <RouterProvider router ={router}/>
 }
 
 export default App
