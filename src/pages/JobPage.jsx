c19e4f78555584ef6b9c6d443f04253dd4228dcb
Pravat <pravatkiran7@gmail.com>
2024-07-31 10:58:39 +0545

    spinner added and useEffect hook added for fetching data from api

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
-}
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default JobPage
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (err) {
+        console.log("error fetching jobs", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;
